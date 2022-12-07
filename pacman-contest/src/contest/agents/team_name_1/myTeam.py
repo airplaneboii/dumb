f008a51a76af3cca04561fe8e2be5ac6417388ed
airplaneboii <88340299+airplaneboii@users.noreply.github.com>
2022-12-07 20:35:24 +0100

    testing different things

--- pacman-contest/src/contest/agents/team_name_1/myTeam.py
+++ pacman-contest/src/contest/agents/team_name_1/myTeam.py
@@ -66,7 +66,7 @@ class DumbAgent(CaptureAgent):
         CaptureAgent.register_initial_state(self, game_state)
     
     def choose_action(self, game_state):
-        print("------------------------------------------------")
+        #print("------------------------------------------------")
         # Najprej pridobi seznam vseh legalnih potez
         actions = game_state.get_legal_actions(self.index)
 
@@ -90,13 +90,42 @@ class DumbAgent(CaptureAgent):
         features = self.get_features(game_state, action)
         weights = self.get_weights(game_state, action)
 
-        if type(self) == LittleGhostie:
-            #print(features)
-            #print(weights)
-            print(features * weights)
+        #if type(self) == LittleGhostie:
+        #    #print(features)
+        #    #print(weights)
+        #    #print(features * weights)
 
         # vrne vsoto produktov istoimenskih komponent
         return features * weights
+
+#class StarvingPaccy(DumbAgent):
+#    def get_features(self, game_state, action):
+#        features = util.Counter()
+#
+#        # postavi se na potencialno naslednjo pozicijo
+#        successor = self.get_successor(game_state, action)
+#
+#        my_state = successor.get_agent_state(self.index)
+#        my_pos = my_state.get_position()
+#
+#        # preveri, na kateri polovici si
+#        if my_state.is_pacman:
+#            # si na nasprotnikovi polovici
+#            print("attack")
+#            
+#        elif my_state.scared_timer > 0:
+#            # izogibaj se pacmanov
+#            print("RUNNN")
+#        
+#        else:
+#            # Pojdi na nasprotnikovo polovico, vmes pa isci, ce je kje kaksen pacman -> ce je, ga napadi
+#            print("Go or eat")
+#            
+#
+#        return features
+#    
+#    def get_weights(self, game_state, action):
+#        return {}
 
 class LittleGhostie(DumbAgent):
     def get_features(self, game_state, action):
@@ -104,36 +133,110 @@ class LittleGhostie(DumbAgent):
 
         # postavi se na potencialno naslednjo pozicijo
         successor = self.get_successor(game_state, action)
-
         my_state = successor.get_agent_state(self.index)
+
+        # pridobi pozicije
+        past_position = None
+        current_position = game_state.get_agent_position(self.index)
         my_pos = my_state.get_position()
 
-        # preveri ali napadas ali branis (upostevaj tudi druge dejavnike, ne samo pozicijo)
-        features['on_defense'] = 1
-        if my_state.is_pacman or my_state.scared_timer > 0:
-            features['on_defense'] = 0
-        
-        # Pridobi razdaljo do nasprotnikov (ce jih vidis)
-        enemies = [successor.get_agent_state(opponent) for opponent in self.get_opponents(successor)]
-        invaders = [enemy for enemy in enemies if enemy.is_pacman and enemy.get_position() is not None]
-        features['num_invaders'] = len(invaders)
-        
-        # Preveri, ali je zmanjkalo kaj hrane
         if self.get_previous_observation() is not None:
-            past_food = self.get_food_you_are_defending(self.get_previous_observation()).as_list()
-            current_food = self.get_food_you_are_defending(game_state).as_list()
-            missing_food = [food for food in past_food if food not in current_food]
+            past_position = self.get_previous_observation().get_agent_position(self.index)
+
+        # pridobi informacije o nasprotniku
+        enemies = [successor.get_agent_state(opponent) for opponent in self.get_opponents(successor)]
+        pacmans = [enemy for enemy in enemies if not enemy.is_pacman and enemy.get_position() is not None]
+        ghosts = [enemy for enemy in enemies if enemy.is_pacman and enemy.get_position() is not None]
+        #print(len(pacmans))
+        # preveri, kaj bos delal
+        if my_state.is_pacman:
+            # si na nasprotnikovi polovici -> ce ti nihce nic ne je, poskusi ti pojest kaj, kar je blizu, a bodi previden
+#            # Preveri, ce ti kdo kaj je
+#            if self.get_previous_observation() is not None:
+#                
+#                past_food = self.get_food_you_are_defending(self.get_previous_observation()).as_list()
+#                current_food = self.get_food_you_are_defending(game_state).as_list()
+#                missing_food = [food for food in past_food if food not in current_food]
+#
+#                if len(missing_food) > 0:
+#                    # nekdo nekaj je, vrni se domov
+#                    missing_food_dist = [self.get_maze_distance(my_pos, food) for food in missing_food]
+#                    features['missing_food_distance'] = min(missing_food_dist)
+#                
+#                enemy_food = self.get_food(game_state).as_list()
+#                #if len(enemy_food) > 2:
+            features['ispacman'] = 0 # delete this
+
             
-            if len(missing_food) > 0:
-                missing_food_dist = [self.get_maze_distance(my_pos, food) for food in missing_food]
-                features['missing_food_distance'] = min(missing_food_dist)
+        elif my_state.scared_timer > 0:
+            # izogibaj se pacmanov in pojdi cim hitreje na nasprotnikovo polovico
+            
+            if len(pacmans) > 0:
+                pacman_distances_future = [self.get_maze_distance(my_pos, pacman.get_position()) for pacman in pacmans]
+                pacman_distances_current = [self.get_maze_distance(current_position, pacman.get_position()) for pacman in pacmans]
+
+                if len(pacman_distances_future) > 0 and len(pacman_distances_current) > 0:
+                    future_min = min(pacman_distances_future)
+                    current_min = min(pacman_distances_current)
+                    features['avoiding_pacman'] = 1 if future_min > current_min else 0
+                    print(features['avoiding_pacman'])
+
+            else:
+                food_list = self.get_food(successor).as_list()
+                food_list_distances = [self.get_maze_distance(my_pos, food) for food in food_list]
+                food_path = min(food_list_distances)
+                features['closest_food'] = food_path
+
+                # preveri, ce se premaknes blizje pacmanu -> TO SE NE ZGODI
         
-        # preveri, kako dalec imas do najblizjega napadalca
-        if len(invaders) > 0:
-            distances = [self.get_maze_distance(my_pos, invader.get_position()) for invader in invaders]
-            features['invader_distance'] = min(distances)
+        else:
+            # poglej, ce ti kdo kaj je, pomakni se nekam v sredino, poskusi z blagim napadom
+            if self.get_previous_observation() is not None:
+                past_food = self.get_food_you_are_defending(self.get_previous_observation()).as_list()
+                current_food = self.get_food_you_are_defending(game_state).as_list()
+                missing_food = [food for food in past_food if food not in current_food]
+
+                if len(missing_food) > 0:
+                    # nekdo nekaj je, poisci ga
+                    missing_food_dist = [self.get_maze_distance(my_pos, food) for food in missing_food]
+                    features['missing_food_distance'] = min(missing_food_dist)
+            
+            # Tu pride Tomazeva funkcija za iskanje hrane
+            features['resting_place'] = 1 # spremeni to glede na to ali te ta smer spravi blizje srediscu ali ne
+            
+
         
-        # Preveri, kje se ti najbolj splaca cakati, ce ni nobenega dogajanja TODO: uporabi Tomazeve funkcije in zgoraj ustvarjene vrednosti
+        
+        
+          
+        
+        
+#        features['on_defense'] = 1
+#        if my_state.is_pacman or my_state.scared_timer > 0:
+#            features['on_defense'] = 0
+#        
+#        # Pridobi razdaljo do nasprotnikov (ce jih vidis)
+#        enemies = [successor.get_agent_state(opponent) for opponent in self.get_opponents(successor)]
+#        invaders = [enemy for enemy in enemies if enemy.is_pacman and enemy.get_position() is not None]
+#        features['num_invaders'] = len(invaders)
+#        
+#        # Preveri, ali je zmanjkalo kaj hrane
+#        if self.get_previous_observation() is not None:
+#            past_food = self.get_food_you_are_defending(self.get_previous_observation()).as_list()
+#            current_food = self.get_food_you_are_defending(game_state).as_list()
+#            missing_food = [food for food in past_food if food not in current_food]
+#            
+#            if len(missing_food) > 0:
+#                missing_food_dist = [self.get_maze_distance(my_pos, food) for food in missing_food]
+#                features['missing_food_distance'] = min(missing_food_dist)
+#        
+#        # preveri, kako dalec imas do najblizjega napadalca
+#        if len(invaders) > 0:
+#            distances = [self.get_maze_distance(my_pos, invader.get_position()) for invader in invaders]
+#            features['invader_distance'] = min(distances)
+#        
+#        # Preveri, kje se ti najbolj splaca cakati, ce ni nobenega dogajanja oziroma naj poskusi z napadom
+#        # TODO: uporabi Tomazeve funkcije in zgoraj ustvarjene vrednosti
         
         # Pojma nimam, kaj je to ...
         if action == Directions.STOP:
